01a64c754f77ba3e680714f4ef5a3036b2e6deda
Adrian Villegas <manbassman1996@gmail.com>
2022-05-26 14:59:46 -0300

    commite

--- back-src/lib/mailer.ts
+++ back-src/lib/mailer.ts
@@ -3,7 +3,7 @@ var nodemailer = require("nodemailer");
 
 export const transporter = nodemailer.createTransport({
   service: "Gmail",
-  host: process.env.HOST_NODEMAILER,
+  host: "smtp.gmail.com",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
